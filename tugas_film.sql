3b18415f882093fb33c5521fd8dee6fed7932d11
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-14 14:58:58 +0700

    Create tugas_film.sql

---
+++ tugas_film.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE tugas_film; 
+
+USE tugas_film;
+ 
+CREATE TABLE film(
+NO TINYINT,
+judul_film ENUM('Avatar','Titanic',
+'Star Wars: The Force Awakens','Jurassic World','The Avengers'),
+pendapatan BIGINT,
+tahun_produksi YEAR(4));
+
+INSERT INTO film VALUES
+(1,'Avatar',2787965087,2009),
+(2,'Titanic',2186772302,1997),
+(3,'Star Wars: The Force Awakens',2068223624,2015),
+(4,'Jurassic World',1671713208,2015),
+(5,'The Avengers',1518812988,2012);
+
+SELECT * FROM film;
